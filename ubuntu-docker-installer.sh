ded0c30f070998d1f5c9b012a0b1edd53bbd6eed
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-10-30 12:18:24 +0100

    Melhorias

--- ubuntu-docker-installer.sh
+++ ubuntu-docker-installer.sh
@@ -1,4 +1,8 @@
+Author: Caio Moreno de Souza (caiomsouza@gmail.com)
+
 # Ubuntu Docker Installer for Ubuntu 14.04
+
+# https://docs.docker.com/linux/step_one/
 
 echo "Welcome to the Docker Installer for Ubunut 14.04"
 
@@ -6,3 +10,8 @@ sudo apt-get update
 
 sudo apt-get -y install wget
 
+sudo wget -qO- https://get.docker.com/ | sh
+
+sudo wget -qO- https://get.docker.com/gpg | sudo apt-key add -
+
+sudo docker run hello-world
